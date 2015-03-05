444abbf65f850007a860a4656aa885c4af3e6ff0
Edward <edpark13@gmail.com>
2015-03-04 17:41:40 -0800

    add delete test

--- imager/profiles/signals.py
+++ imager/profiles/signals.py
@@ -6,12 +6,8 @@ from models import ImagerProfile
 @receiver(post_save, sender=User)
 def create_profile(sender, **kwargs):
     if kwargs.get('created', False):
-        try:
-            ImagerProfile(user=kwargs.get('instance')).save()
-        except (KeyError, ValueError):
-            msg = "Unable to create ImagerProfile or {}"
-            # logger.error(msg.format(kwargs['instance']))
+        ImagerProfile(user=kwargs.get('instance')).save()
+
 @receiver(pre_delete, sender=User)
 def delete_profile(sender, **kwargs):
-    profile = ImagerProfile.objects.get(user=kwargs.get('instance'))
-    profile.delete()
+    ImagerProfile.objects.get(user=kwargs.get('instance')).delete()
