0da0bf1f7143b7ad8f6ed3398b5455cbdc3d52ca
Edward <edpark13@gmail.com>
2015-03-08 12:17:30 -0700

    add photos model

--- imager/imager_images/models.py
+++ imager/imager_images/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Photo(models.Model):
+    title = models.CharField(max_length=40)
+    description = models.TextField()
+    date_uploaded = models.DateField(auto_now_add=True)
+    date_modified = models.DateField(auto_now=True)
+    date_published = models.DateField(auto_now=True)
+    published = models.CharField(choices=(
+        ('public', 'public'),
+        ('private', 'private'),
+        ('shared', 'shared')), default='private')
+    profile = models.ForeignField('ImagerProfile', symmetrical=True, related_name='photos')
+    
+
+class Albums(models.Model):
+    pass
