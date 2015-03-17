5161d9b06bc44c1584413e795d84cc62b7a6be22
Edward <edpark13@gmail.com>
2015-03-16 17:30:26 -0700

    fix edit profile

--- imager/profiles/views.py
+++ imager/profiles/views.py
@@ -2,19 +2,15 @@ from django.views.generic import UpdateView
 from profiles.models import ImagerProfile
 from django.core.urlresolvers import reverse_lazy
 from django.contrib.auth.views import redirect_to_login
-# @login_required
-class ImagerProfileUpdateView(UpdateView):
-    def user_passes_test(self, request):
-        if request.user.is_authenticated():
-            self.object = self.get_object()
-            return self.object.user == request.user
-        return False
+from django.shortcuts import redirect
 
+class ImagerProfileUpdateView(UpdateView):
     def dispatch(self, request, *args, **kwargs):
-        if not self.user_passes_test(request):
-            return redirect_to_login(request.get_full_path())
-        return super(ImagerProfileUpdateView, self).dispatch(
-            request, *args, **kwargs)
+        profile = ImagerProfile.objects.get(id=int(self.kwargs['pk']))
+        user_profile = ImagerProfile.objects.get(user=self.request.user)
+        if profile != user_profile:
+            return redirect('/accounts/login/')
+        return super(ImagerProfileUpdateView, self).dispatch(request, *args, **kwargs)
 
 
     model = ImagerProfile
